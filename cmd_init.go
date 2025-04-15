dd40d72bd0192ad90867927c2653a1f1cafbca80
makki_d <d.makiuchi@gmail.com>
2025-04-16 00:29:59 +0900

    cmd init

--- cmd_init.go
+++ cmd_init.go
@@ -1,24 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
+
+var initOverwrite bool
 
 // cmdInit represents the init command
 var cmdInit = &cobra.Command{
 	Use:   "init",
 	Short: "Generate the initial migration SQL file",
-	Long: `The init command generates the first migration SQL file.
+	Long: `Generate the initial migration SQL file.
+This file sets up the initial state of the database,
+including the _migrations table used to track applied migrations.`,
 
-This file sets up the initial state of the database, including the _migrations table used to track applied migrations.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init!")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return generateInitSQLFile(targetDir, initOverwrite)
 	},
 }
 
 func init() {
 	cmd.AddCommand(cmdInit)
-	cmdInit.Flags().BoolP("force", "f", false, "Override the output file if it exists")
+	cmdInit.Flags().BoolVarP(&initOverwrite, "force", "f", false, "Override the output file if it exists")
+}
+
+const initFile = "000000_init.all.sql"
+const initSQL = `CREATE TABLE _migrations (
+   id      bigint NOT NULL,
+   applied datetime,
+   title   varchar(255),
+   PRIMARY KEY (id)
+);
+
+DELIMITER //
+
+CREATE PROCEDURE _migration_exists(IN input_id INT)
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM migrations WHERE id = input_id) THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'migration not found';
+    END IF;
+END //
+
+DELIMITER ;
+`
+
+func generateInitSQLFile(dir string, overwrite bool) error {
+	path := filepath.Join(dir, initFile)
+	flag := os.O_CREATE | os.O_RDWR
+	if !overwrite {
+		flag |= os.O_EXCL
+	}
+	f, err := os.OpenFile(path, flag, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write([]byte(initSQL))
+	return err
 }
--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import "github.com/spf13/cobra"
 
 var cmd = &cobra.Command{
-	Use:   "migy",
-	Short: "A standalone database migration helper for MySQL",
-	Long:  "",
+	Use:           "migy",
+	Short:         "A standalone database migration helper for MySQL",
+	Long:          "",
+	SilenceErrors: true,
+	SilenceUsage:  true,
+}
+
+var (
+	targetDir string
+)
+
+func init() {
+	cmd.PersistentFlags().StringVarP(&targetDir, "dir", "d", ".", "directory with migration files")
 }
 
 func main() {
